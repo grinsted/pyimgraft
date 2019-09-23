20c474d431d166b4ee7b603e511390255a15ce59
Aslak Grinsted <ag@glaciology.net>
2019-09-23 13:47:37 +0200

    support multiple bands

--- geoimread.py
+++ geoimread.py
@@ -8,19 +8,9 @@ Created on Thu Apr 25 11:58:22 2019
 import xarray as xr
 import numpy as np
 from rasterio.warp import transform
-from typing import List
-
-Vector = List[float]
 
 
-def geoimread(
-    fname: str,
-    roi_x: Vector = None,
-    roi_y: Vector = None,
-    roi_crs: dict = None,
-    buffer: float = 0.0,
-    band: int = 1,
-) -> xr.core.dataarray.DataArray:
+def geoimread(fname, roi_x=None, roi_y=None, roi_crs=None, buffer=0, band=0):
     """Reads a sub-region of a geotiff/geojp2/ that overlaps a region of interest (roi)
 
     This is a simple wrapper of xarray functionality.
@@ -42,14 +32,20 @@ def geoimread(
             roi_x = [roi_x]
             roi_y = [roi_y]
         if roi_crs is not None:
+            if str(roi_crs) == "LL":
+                roi_crs = {"init": "EPSG:4326"}
+            # remove nans 
+            roi_x = np.array(roi_x)
+            roi_y = np.array(roi_y)
+            ix = ~np.isnan(roi_x+roi_y)
             roi_x, roi_y = transform(
-                src_crs=roi_crs, dst_crs=da.crs, xs=roi_x, ys=roi_y
+                src_crs=roi_crs, dst_crs=da.crs, xs=roi_x[ix], ys=roi_y[ix]
             )
         rows = (da.y > np.min(roi_y) - buffer) & (da.y < np.max(roi_y) + buffer)
         cols = (da.x > np.min(roi_x) - buffer) & (da.x < np.max(roi_x) + buffer)
-        return da[band - 1, rows, cols].squeeze()
+        return da[band, rows, cols].squeeze()
     else:
-        return da[band - 1, :, :].squeeze()
+        return da[band, :, :].squeeze()
 
 
 if __name__ == "__main__":
@@ -57,7 +53,7 @@ if __name__ == "__main__":
     # Read the data
     fA = "https://storage.googleapis.com/gcp-public-data-landsat/LC08/01/023/001/LC08_L1TP_023001_20150708_20170407_01_T1/LC08_L1TP_023001_20150708_20170407_01_T1_B8.TIF"
     A = geoimread(
-        fA, roi_x=[-30.19], roi_y=[81.245], roi_crs={"init": "EPSG:4326"}, buffer=20000
+        fA, roi_x=[-30.19, np.nan], roi_y=[81.245, np.nan], roi_crs={"init": "EPSG:4326"}, buffer=20000
     )
 
     import matplotlib.pyplot as plt
